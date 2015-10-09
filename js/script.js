99655ef2c07412c8b30f89d173b4bae645d93fb9
Denis1990 <den.bogdanov4@gmail.com>
2015-10-10 00:34:56 +0300

    стилизовал слайдер

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     slideSpeed : 300,
     pagination : false,
     paginationSpeed : 400,
-    singleItem:true
+    singleItem:true,
+    navigationText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
   });
 })
