0713b4dffe21b42999dcc95e186525597ea7e822
Denis1990 <den.bogdanov4@gmail.com>
2015-10-13 22:59:16 +0300

    добавил внутреннюю страницу портфолио

--- js/script.js
+++ js/script.js
@@ -7,4 +7,18 @@ $(document).ready(function() {
     singleItem:true,
     navigationText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
   });
+   $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    focusOnSelect: true
+  });
 })
